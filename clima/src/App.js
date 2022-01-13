350a4753b66812b59f395c780dea1830d3e4a1b4
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-13 11:54:32 -0600

    Se creo el Formulario

--- clima/src/App.js
+++ clima/src/App.js
@@ -1,8 +1,29 @@
-
+import { Fragment } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
-    <h1>Clima</h1>
+    <Fragment>
+        <Header
+            titulo='Clima React App' />
+
+        <div className="contenedor-form">
+          <div className="container">
+            <div className='row'>
+
+              <div className='col m6 s12'>
+                <Formulario />
+              </div>
+
+              <div className='col m6 s12'>
+                2
+              </div>
+
+            </div>
+          </div>
+        </div>
+    </Fragment>
   );
 }
 
